416ecb265efecaa9c41745b4d3b927ee87c67eb9
flavio_seixas@id.uff.br <flavio_seixas@id.uff.br>
2019-02-02 23:23:30 -0200

    v0

---
+++ DP-C1/src/dp/c1/MiniDuckSimulator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.c1;
+
+/**
+ *
+ * @author fseixas
+ */
+public class MiniDuckSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performFly();
+        mallard.performQuack();
+        
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+    }
+    
+}
